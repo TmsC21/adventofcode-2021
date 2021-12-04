becdc25fc224a7ed9bafe462aef84f8be6da4699
TmsC21 <t.cupela21@gmail.com>
2021-12-04 13:35:53 +0100

    Implement day 4

--- src/Main.java
+++ src/Main.java
@@ -3,93 +3,139 @@ import java.util.*;
 
 
 public class Main {
+    private static final int MAXLEN = 5;
+    private static int BOARDLEN;
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<String>();
         File file = new File("input.txt");
-        try(BufferedReader br = new BufferedReader(new FileReader(file))) {
-            for(String line; (line = br.readLine()) != null; ) {
-                list.add(line);
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            for (String line; (line = br.readLine()) != null; ) {
+                if(!line.isEmpty()){
+                    list.add(line);
+                }
             }
-        }catch (Exception ignored){}
-        System.out.println(getPwrConsum(list));
-        System.out.println(getRating(new ArrayList<String>(list),true)*getRating(new ArrayList<String>(list),false));
+        } catch (Exception ignored) {
+        }
+        ArrayList<Integer> listDrawNumbers = getListOfDrawNumbers(list.get(0));
+        ArrayList<Integer[][]> listOfBoards = getListOfBoards(getListOfBoardNumbers(list),false);
+        ArrayList<Integer[][]> listOfPlayBoards = getListOfBoards(getListOfBoardNumbers(list),true);
+        BOARDLEN = listOfBoards.size();
+        int y = 0,x = 5;
+        ArrayList<Integer> drawNumbers = new ArrayList<Integer>();
+        for (int i = 0; i < listDrawNumbers.size(); i++,y++) {
+            if(y == x){
+                checkBoards(listOfBoards,listOfPlayBoards,drawNumbers);
+                x++;
+                y = 0;
+                drawNumbers = new ArrayList<Integer>();
+            }
+            drawNumbers.add(listDrawNumbers.get(i));
+        }
     }
-    static int getRating(ArrayList<String> list, boolean isOxygen){
-        int one = 0, zero = 0, len = list.get(0).length();
-        String str = "";
-        for (int i = 0; i < len; i++) {
-            Iterator<String> iter = list.iterator();
-            for (int y = 0; y < list.size(); y++) {
-                if (list.get(y).charAt(i) == '1') {
-                    one++;
-                } else {
-                    zero++;
-                }
-                if (y == list.size() - 1) {
-                    if(isOxygen){
-                        if (one > zero) {
-                            str = str + "1";
-                        } else if (one == zero) {
-                            str = str + "1";
-                        } else {
-                            str = str + "0";
-                        }
-                    }else{
-                        if (one > zero) {
-                            str = str + "0";
-                        } else if (one == zero) {
-                            str = str + "0";
-                        } else {
-                            str = str + "1";
+    static void checkBoards(ArrayList<Integer[][]> boards,ArrayList<Integer[][]> playableBoards ,ArrayList<Integer> drawNumbers){
+        int sum = 0;
+        for (int i = 0; i < drawNumbers.size(); i++) {
+            Iterator<Integer[][]> iterator = boards.iterator();
+            int j = 0;
+            while(iterator.hasNext()){
+                Integer[][] board = iterator.next();
+                for (int k = 0; k < MAXLEN; k++) {
+                    for (int l = 0; l < MAXLEN; l++) {
+                        if(board[k][l].equals(drawNumbers.get(i))){
+                            playableBoards.get(j)[k][l] = drawNumbers.get(i);
+                            if((sum = checkForWinningBoard(board,playableBoards.get(j),drawNumbers.get(i))) > 0){
+                                if(boards.size() == BOARDLEN || boards.size() == 1){
+                                    System.out.println("Result: "+sum);
+                                }
+                                iterator.remove();
+                                playableBoards.remove(playableBoards.get(j));
+                                j--;
+                            }
                         }
                     }
-                    one = 0;
-                    zero = 0;
                 }
-            }
-            if (str.charAt(i) == '1') {
-                while (iter.hasNext()) {
-                    if (iter.next().charAt(i) != '1') {
-                        iter.remove();
-                    }
-                }
-            } else {
-                while (iter.hasNext()) {
-                    if (iter.next().charAt(i) != '0') {
-                        iter.remove();
-                    }
-                }
-            }
-            if(list.size() <= 1){
-                break;
+                j++;
             }
         }
-        return Integer.parseInt(list.get(0),2);
     }
-    static int getPwrConsum(ArrayList<String> list){
-        int one = 0, zero = 0, len = list.get(0).length();
-        String gamma  = "";
-        String epsilon = "";
-        for (int i = 0; i < len; i++) {
-            for (int y = 0; y < list.size(); y++) {
-                if (list.get(y).charAt(i) == '1') {
-                    one++;
-                } else {
-                    zero++;
+    static int checkForWinningBoard(Integer[][] board,Integer[][] playableBoards, int drawNumber){
+        int horizontal = 0, vertical = 0;
+        for (int j = 0; j < MAXLEN; j++) {
+            for (int k = 0; k < MAXLEN; k++) {
+                if (!playableBoards[j][k].equals(-1)) {
+                    vertical++;
                 }
-                if (y == list.size() - 1) {
-                    if (one > zero) {
-                        gamma  = gamma + "1";
-                        epsilon = epsilon + "0";
-                    } else {
-                        gamma  = gamma + "0";
-                        epsilon = epsilon + "1";
-                    }
-                    one = 0;
-                    zero = 0;
+                if (vertical == 5) {
+                    return calculateWinner(playableBoards, board, drawNumber);
+                }
+                if (!playableBoards[k][j].equals(-1)) {
+                    horizontal++;
+                }
+                if (horizontal == 5) {
+                    return calculateWinner(playableBoards, board, drawNumber);
+                }
+            }
+            vertical = 0;
+            horizontal = 0;
+        }
+        return -1;
+    }
+    static int calculateWinner(Integer[][] winningBoard,Integer[][] board,int drawNumber){
+        int sum = 0;
+        for (int k = 0; k < MAXLEN; k++) {
+            for (int l = 0; l < MAXLEN; l++) {
+                if(!board[k][l].equals(winningBoard[k][l])){
+                    sum += board[k][l];
                 }
             }
         }
-        return Integer.parseInt(gamma,2) * Integer.parseInt(epsilon,2);
+        return sum * drawNumber;
+    }
+
+    static ArrayList<Integer> getListOfDrawNumbers(String str){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        String[] arrStr = str.split(",");
+        for (String s : arrStr){
+            list.add(Integer.parseInt(s));
+        }
+        return list;
+    }
+
+    static ArrayList<Integer> getListOfBoardNumbers(ArrayList<String> list){
+        ArrayList<Integer> listBoardNumber = new ArrayList<Integer>();
+        for (int i = 1; i < list.size(); i++) {
+            String[] arrStr = list.get(i).split(" ");
+            for (String s : arrStr){
+                if(!s.isEmpty()){
+                    listBoardNumber.add(Integer.parseInt(s));
+                }
+            }
+        }
+        return listBoardNumber;
+    }
+
+    static ArrayList<Integer[][]> getListOfBoards(ArrayList<Integer> list,boolean isEmpty){
+        ArrayList<Integer[][]> listOfBoards = new ArrayList<>();
+        int y = 0;
+        Integer[][] board = new Integer[MAXLEN][MAXLEN];
+        for (int i = 0; i < list.size(); i++) {
+            if(i%5 == 0 && i != 0){
+                y++;
+            }
+            if(y == 5){
+                listOfBoards.add(board);
+                y = 0;
+                board = new Integer[MAXLEN][MAXLEN];
+            }
+            if(isEmpty) {
+                board[y][i%5] = -1;
+            }else{
+                board[y][i%5] = list.get(i);
+            }
+            if(i == list.size() - 1){
+                listOfBoards.add(board);
+            }
+        }
+        return listOfBoards;
     }
 }
