d2c258b6fc9c9fcd91247f525557a99eca52666d
TmsC21 <t.cupela21@gmail.com>
2021-12-05 15:48:03 +0100

    Implement day 5

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,29 @@
 import java.io.*;
 import java.util.*;
 
+class LineSegment{
+    Coordinates start;
+    Coordinates end;
+    LineSegment(Coordinates start, Coordinates end){
+        this.start = start;
+        this.end = end;
+    }
+    public void printCor(){
+        System.out.println(start.x +","+end.x+" => "+start.y+","+end.y);
+    }
+}
 
+class Coordinates{
+    int x;
+    int y;
+    Coordinates(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
 public class Main {
-    private static final int MAXLEN = 5;
-    private static int BOARDLEN;
+    private static int XLEN;
+    private static int YLEN;
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<String>();
         File file = new File("input.txt");
@@ -16,126 +35,137 @@ public class Main {
             }
         } catch (Exception ignored) {
         }
-        ArrayList<Integer> listDrawNumbers = getListOfDrawNumbers(list.get(0));
-        ArrayList<Integer[][]> listOfBoards = getListOfBoards(getListOfBoardNumbers(list),false);
-        ArrayList<Integer[][]> listOfPlayBoards = getListOfBoards(getListOfBoardNumbers(list),true);
-        BOARDLEN = listOfBoards.size();
-        int y = 0,x = 5;
-        ArrayList<Integer> drawNumbers = new ArrayList<Integer>();
-        for (int i = 0; i < listDrawNumbers.size(); i++,y++) {
-            if(y == x){
-                checkBoards(listOfBoards,listOfPlayBoards,drawNumbers);
-                x++;
-                y = 0;
-                drawNumbers = new ArrayList<Integer>();
-            }
-            drawNumbers.add(listDrawNumbers.get(i));
+        ArrayList<LineSegment> linesSegmentList = getCoordinates(list);
+        String[][] gameTable = getGameTable();
+        for (int i = 0; i < linesSegmentList.size(); i++) {
+            int XCOR =linesSegmentList.get(i).end.x - linesSegmentList.get(i).start.x;
+            int YCOR =linesSegmentList.get(i).end.y - linesSegmentList.get(i).start.y;
+                if(XCOR > 0 && YCOR > 0){
+                    encreaseX(linesSegmentList.get(i).start.x,linesSegmentList.get(i).end.x,linesSegmentList.get(i).start.y,linesSegmentList.get(i).end.y,gameTable,true,true);
+                }else if(XCOR > 0 && YCOR < 0){
+                    encreaseX(linesSegmentList.get(i).start.x,linesSegmentList.get(i).end.x,linesSegmentList.get(i).start.y,linesSegmentList.get(i).end.y,gameTable,true,false);
+                }else if(XCOR < 0 && YCOR > 0){
+                    dencreaseX(linesSegmentList.get(i).start.x,linesSegmentList.get(i).end.x,linesSegmentList.get(i).start.y,linesSegmentList.get(i).end.y,gameTable,true,true);
+                }else if(XCOR < 0 && YCOR < 0) {
+                    dencreaseX(linesSegmentList.get(i).start.x,linesSegmentList.get(i).end.x,linesSegmentList.get(i).start.y,linesSegmentList.get(i).end.y,gameTable,true,false);
+                }else if(XCOR > 0) {
+                    encreaseX(linesSegmentList.get(i).start.x,linesSegmentList.get(i).end.x,linesSegmentList.get(i).start.y,linesSegmentList.get(i).end.y,gameTable,false,false);
+                } else if(XCOR < 0) {
+                    dencreaseX(linesSegmentList.get(i).start.x,linesSegmentList.get(i).end.x,linesSegmentList.get(i).start.y,linesSegmentList.get(i).end.y,gameTable,false,false);
+                }else if(YCOR > 0) {
+                    encreaseY(linesSegmentList.get(i).start.y,linesSegmentList.get(i).end.y,linesSegmentList.get(i).start.x,gameTable,false);
+                }else if(YCOR < 0) {
+                    dencreaseY(linesSegmentList.get(i).start.y,linesSegmentList.get(i).end.y,linesSegmentList.get(i).start.x,gameTable,false);
+                }else{
+                    encreaseY(XCOR,YCOR,YCOR,gameTable,false);
+                }
         }
-    }
-    static void checkBoards(ArrayList<Integer[][]> boards,ArrayList<Integer[][]> playableBoards ,ArrayList<Integer> drawNumbers){
-        int sum = 0;
-        for (int i = 0; i < drawNumbers.size(); i++) {
-            Iterator<Integer[][]> iterator = boards.iterator();
-            int j = 0;
-            while(iterator.hasNext()){
-                Integer[][] board = iterator.next();
-                for (int k = 0; k < MAXLEN; k++) {
-                    for (int l = 0; l < MAXLEN; l++) {
-                        if(board[k][l].equals(drawNumbers.get(i))){
-                            playableBoards.get(j)[k][l] = drawNumbers.get(i);
-                            if((sum = checkForWinningBoard(board,playableBoards.get(j),drawNumbers.get(i))) > 0){
-                                if(boards.size() == BOARDLEN || boards.size() == 1){
-                                    System.out.println("Result: "+sum);
-                                }
-                                iterator.remove();
-                                playableBoards.remove(playableBoards.get(j));
-                                j--;
-                            }
-                        }
+        int count = 0;
+        for (int y = 0; y < YLEN; y++) {
+            for (int x = 0; x < XLEN; x++) {
+                if(!gameTable[y][x].equals(".")){
+                    if(Integer.parseInt(gameTable[y][x]) > 1){
+                        count++;
                     }
                 }
-                j++;
             }
         }
+        System.out.println(count);
     }
-    static int checkForWinningBoard(Integer[][] board,Integer[][] playableBoards, int drawNumber){
-        int horizontal = 0, vertical = 0;
-        for (int j = 0; j < MAXLEN; j++) {
-            for (int k = 0; k < MAXLEN; k++) {
-                if (!playableBoards[j][k].equals(-1)) {
-                    vertical++;
+    static void encreaseX(int xstart, int xend, int ystart,int yend, String[][] gameTable, boolean isXY, boolean isEncreaseY){
+        if(isXY){
+            if(isEncreaseY){
+                for (int x = xstart,y = ystart; x <= xend; x++,y++) {
+                    encreaseY(y,yend,x,gameTable,true);
                 }
-                if (vertical == 5) {
-                    return calculateWinner(playableBoards, board, drawNumber);
-                }
-                if (!playableBoards[k][j].equals(-1)) {
-                    horizontal++;
-                }
-                if (horizontal == 5) {
-                    return calculateWinner(playableBoards, board, drawNumber);
-                }
-            }
-            vertical = 0;
-            horizontal = 0;
-        }
-        return -1;
-    }
-    static int calculateWinner(Integer[][] winningBoard,Integer[][] board,int drawNumber){
-        int sum = 0;
-        for (int k = 0; k < MAXLEN; k++) {
-            for (int l = 0; l < MAXLEN; l++) {
-                if(!board[k][l].equals(winningBoard[k][l])){
-                    sum += board[k][l];
-                }
-            }
-        }
-        return sum * drawNumber;
-    }
-
-    static ArrayList<Integer> getListOfDrawNumbers(String str){
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        String[] arrStr = str.split(",");
-        for (String s : arrStr){
-            list.add(Integer.parseInt(s));
-        }
-        return list;
-    }
-
-    static ArrayList<Integer> getListOfBoardNumbers(ArrayList<String> list){
-        ArrayList<Integer> listBoardNumber = new ArrayList<Integer>();
-        for (int i = 1; i < list.size(); i++) {
-            String[] arrStr = list.get(i).split(" ");
-            for (String s : arrStr){
-                if(!s.isEmpty()){
-                    listBoardNumber.add(Integer.parseInt(s));
-                }
-            }
-        }
-        return listBoardNumber;
-    }
-
-    static ArrayList<Integer[][]> getListOfBoards(ArrayList<Integer> list,boolean isEmpty){
-        ArrayList<Integer[][]> listOfBoards = new ArrayList<>();
-        int y = 0;
-        Integer[][] board = new Integer[MAXLEN][MAXLEN];
-        for (int i = 0; i < list.size(); i++) {
-            if(i%5 == 0 && i != 0){
-                y++;
-            }
-            if(y == 5){
-                listOfBoards.add(board);
-                y = 0;
-                board = new Integer[MAXLEN][MAXLEN];
-            }
-            if(isEmpty) {
-                board[y][i%5] = -1;
             }else{
-                board[y][i%5] = list.get(i);
+                for (int x = xstart,y = ystart; x <= xend; x++,y--) {
+                    dencreaseY(y,yend,x,gameTable,true);//done
+                }
             }
-            if(i == list.size() - 1){
-                listOfBoards.add(board);
+        }else{
+            for (int x = xstart; x <= xend; x++) {
+                gameTable[ystart][x] = chechNode(gameTable[ystart][x]);
             }
         }
-        return listOfBoards;
+    }
+    static void dencreaseX(int xstart, int xend, int ystart,int yend, String[][] gameTable,boolean isXY, boolean isEncreaseY){
+        if(isXY){
+            if(isEncreaseY){
+                for (int x = xstart,y = ystart; x >= xend; x--,y++) {
+                    encreaseY(y,yend,x,gameTable,true);
+                }
+            }else{
+                for (int x = xstart,y = ystart; x >= xend; x--,y--) {
+                    dencreaseY(y,yend,x,gameTable,true);
+                }
+            }
+        }else{
+            for (int x = xstart; x >= xend; x--) {
+                gameTable[ystart][x] = chechNode(gameTable[ystart][x]);
+            }
+        }
+    }
+    static void encreaseY(int ystart,int yend, int x,String[][] gameTable,boolean isXY){
+        if(isXY){
+            gameTable[ystart][x] = chechNode(gameTable[ystart][x]);
+        }else{
+            for (int y = ystart; y <= yend; y++) {
+                gameTable[y][x] = chechNode(gameTable[y][x]);
+            }
+        }
+    }
+    static void dencreaseY(int ystart,int yend, int x,String[][] gameTable,boolean isXY){
+        if(isXY){
+            gameTable[ystart][x] = chechNode(gameTable[ystart][x]);
+        }else{
+            for (int y = ystart; y >= yend; y--) {
+                gameTable[y][x] = chechNode(gameTable[y][x]);
+            }
+        }
+    }
+
+    static String chechNode(String str){
+        if(str.equals(".")){
+            return "1";
+        }else{
+            int num = Integer.parseInt(str);
+            return String.valueOf(++num);
+        }
+    }
+    static String[][] getGameTable(){
+        String[][] table = new String[YLEN][XLEN];
+        for (int y = 0; y < YLEN; y++) {
+            for (int x = 0; x < XLEN; x++) {
+                table[y][x] = ".";
+            }
+        }
+        return table;
+    }
+
+    static ArrayList<LineSegment> getCoordinates(ArrayList<String> list){
+        ArrayList<LineSegment> myList = new ArrayList<>();
+        int biggestX = 0, biggestY = 0;
+        for (String line: list) {
+            String[] str = line.split(" -> ");
+            String[] left = str[0].split(",");
+            String[] right = str[1].split(",");
+            myList.add(new LineSegment(new Coordinates(Integer.parseInt(left[0]),Integer.parseInt(left[1])),new Coordinates(Integer.parseInt(right[0]),Integer.parseInt(right[1]))));
+            if(Integer.parseInt(left[0]) > biggestX){
+                biggestX = Integer.parseInt(left[0]);
+            }
+            if(Integer.parseInt(right[0]) > biggestX){
+                biggestX = Integer.parseInt(right[0]);
+            }
+            if(Integer.parseInt(left[1]) > biggestY){
+                biggestY = Integer.parseInt(left[1]);
+            }
+            if(Integer.parseInt(right[1]) > biggestY){
+                biggestY = Integer.parseInt(right[1]);
+            }
+        }
+        XLEN = biggestX+1;
+        YLEN = biggestY+1;
+        return myList;
     }
 }
